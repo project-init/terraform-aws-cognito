022dc7e8ded341230ee3815496c9834b33b1dd55
Derek Rushing <20804720+drush211@users.noreply.github.com>
2026-01-06 08:19:27 -0600

    examples

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -3,12 +3,27 @@ module "cognito" {
   # Project Init recommends pinning every module to a specific version
   # version = "vX.X.X"
 
-  environment                = "staging"
-  service_name               = "business-platform"
+  environment                = "environment"
+  service_name               = "service_name"
   enable_deletion_protection = true
 
+  custom_attributes = [
+    {
+      data_type = "Boolean"
+      name      = "variable"
+      mutable   = true
+      required  = false
+    }
+  ]
+
+  cognito_via_managed_login = false
   google_auth = {
-    client_id     = "client_id"
-    client_secret = "client_secret"
+    client_id     = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["googleOauth2ClientId"]
+    client_secret = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["googleOauth2ClientSecret"]
   }
+
+  callback_urls = compact([
+    "callback_url",
+    var.add_localhost_auth_callback_url ? "localhost_callback_url" : ""
+  ])
 }
